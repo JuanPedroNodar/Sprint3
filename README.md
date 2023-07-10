84eeaa359fa42fc8c56d5b1175e7b83b34d511a5
¨JuanPedroNodar¨ <juanpedro.nodar@gmail.com>
2023-07-10 17:09:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Desafío
+
+- Sprint 3
+
+Tercer proyecto creado en Senpai Academy
+
+-HTML, CSS and JavaScript
+-DOM
+
+### Comportamiento esperado
+
+- Validar los campos del formulario al enviarlo
+- Si no hay errores, mostrar el estado completado
+- Restablecer el formulario cuando el usuario haga clic en "Continuar" en el estado completado
+- Actualizar los datos de la tarjeta a medida que el usuario rellena los campos (Punto extra)
+
+### Los usuarios deben poder:
+
+- Recibir mensajes de error al enviar el formulario si:
+  - Cualquier campo está vacío
+  - Los campos de número de tarjeta, fecha de caducidad o CVC tienen un formato incorrecto.
+- Ver el diseño óptimo en función del tamaño de la pantalla de su dispositivo
+- Rellenar el formulario y ver cómo se actualizan los datos de la tarjeta en tiempo real.(Punto extra)
---
+++ javascript.js
@@ -0,0 +1,99 @@
+
+
+      const sectionInput = document.getElementById("sectionInput");
+      const cardName = document.getElementById("cardName");
+      const cardNumber = document.getElementById("cardNumber");
+      const cardMM = document.getElementById("cardMM");
+      const cardYY = document.getElementById("cardYY");
+      const cardCVC = document.getElementById("cardCVC");
+      const confirmButton = document.getElementById("confirmButton");
+      const outPutName = document.getElementById("outPutName");
+      const cardNameExample = document.querySelector(".cardNameExample");
+      const cardNumberExample = document.querySelector(".cardNumberExample");
+      const cardMonthExample = document.querySelector(".cardMonthExample");
+      const cardYearExample = document.querySelector(".cardYearExample");
+      const cardCvcExample = document.querySelector(".cardCvcExample");
+
+      confirmButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (
+          cardName.value &&
+          cardNumber.value &&
+          cardMM.value &&
+          cardYY.value &&
+          cardCVC.value
+        ) {
+          alert("Todos los campos están llenos");
+          return;
+        } else {
+          alert("Faltan campos por llenar");
+        }
+
+      });
+
+      confirmButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (cardMM.value <= 0  || cardMM.value > 12) {
+            alert("Mes no válido")
+        } else {}
+        
+      });
+
+      confirmButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (cardYY.value <= 23  || cardYY.value > 33) {
+            alert("Año no válido")
+        } else {}
+        
+      });
+
+     
+      cardName.addEventListener("input", () => {
+        if (cardName.value == "") {
+          cardNameExample.innerText = "JANE APPLESED";
+        } else {
+          cardNameExample.innerText = cardName.value;
+        }
+      });
+
+      cardNumber.addEventListener("input", () => {
+        if (cardNumber.value === "") {
+          cardNumberExample.innerText = "0000 0000 0000 0000";
+        } else {
+          cardNumberExample.innerText = cardNumber.value;
+        }
+      });
+
+      cardMM.addEventListener("input", () => {
+        if (cardMM.value == "") {
+          cardMonthExample.innerText = "00";
+        } else {
+          cardMonthExample.innerText = cardMM.value;
+        }
+      });
+      
+      cardYY.addEventListener("input", () => {
+        if (cardYY.value == "") {
+          cardYearExample.innerText = "00";
+        } else {
+            cardYearExample.innerText = cardYY.value;
+        }
+      });
+
+      cardCVC.addEventListener("input", () => {
+        if (cardCVC.value == "") {
+          cardCvcExample.innerText = "000";
+        } else {
+            cardCvcExample.innerText = "***";
+        }
+      });
+
+     /*  function formatNumber() {
+        let numeros4 = cardNumber.value;
+        let cleanedValue = numeros4.replace(/\D/g, "");
+        let truncatedValue = cleanedValue.slice(0, 16); 
+        let groups = truncatedValue.match(/.{1,4}/g); 
+        let formattedValue = groups ? groups.join(" ") : "";
+        cardNumber.value = formattedValue;
+      }
+       */
